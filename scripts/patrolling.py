136e0eb163ebef57f7552c8740fa8d207d723464
Connor <connor.york000@gmail.com>
2023-04-18 16:20:22 +0100

    anotha one

--- scripts/patrolling.py
+++ scripts/patrolling.py
@@ -6,7 +6,7 @@ import pandas as pd
 import rospkg
 import actionlib
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
-from actionlib_msgs.msg import GoalStatus
+from actionlib_msgs.msg import GoalStatus, GoalStatusArray
 from geometry_msgs.msg import Pose, Point, Quaternion
 from tf.transformations import quaternion_from_euler
 
@@ -66,7 +66,7 @@ class Patroller():
 
         #Initiate status subscriber 
         self.status_subscriber = rospy.Subscriber(
-            "/move_base/status/status_list", GoalStatus, self.status_cb
+            "/move_base/status", GoalStatusArray, self.status_cb
         )
 
         self.movebase_client()
