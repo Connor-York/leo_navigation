c5069c8c4148e786f246a843cf7af304b035ed06
Connor <connor.york000@gmail.com>
2023-05-03 16:13:20 +0100

    ud

--- src/test.py
+++ src/test.py
@@ -12,27 +12,11 @@ def spin_robot():
     # Create a Twist publisher to send velocity commands to the robot
     vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
 
-    # Create a MoveBase client to send goal requests to move_base
-    move_base_client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
-    move_base_client.wait_for_server()
-
-    # Set up goal position and orientation for move_base
-    #goal = MoveBaseGoal()
-    #goal.target_pose.header.frame_id = "map"
-    #goal.target_pose.header.stamp = rospy.Time.now()
-   # goal.target_pose.pose.orientation.w = 1.0
-    rospy.loginfo("HERE HERE HERE")
     # Spin robot on the spot for 10 seconds
     t_end = rospy.Time.now() + rospy.Duration(10)
     while rospy.Time.now() < t_end:
-        rospy.loginfo("HERE 2 ")
-        #goal.target_pose.pose.position.x = 0
-        #goal.target_pose.pose.position.y = 0
-        #goal.target_pose.pose.position.z = 0
-        #move_base_client.send_goal(goal)
-        #move_base_client.wait_for_result()
         vel_msg = Twist()
-        vel_msg.angular.z = 10.0
+        vel_msg.angular.z = 5.0
         vel_pub.publish(vel_msg)
 
     # Stop the robot
