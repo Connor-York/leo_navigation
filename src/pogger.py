d0fc28c9460bdffd9abe7fa87673c6c7bf989811
Jay <Jonas.Hamill@bristol.ac.uk>
2023-11-09 22:38:57 +0000

    using strip

--- src/pogger.py
+++ src/pogger.py
@@ -57,9 +57,9 @@ def battery_callback(msg):
     save_to_csv(battery_path,data)
 
 def metrics_callback(msg):
-    data = [msg.data]
-    data = data.replace('"', '')
-    save_to_csv(metrics_path, data)
+    data = msg.data.strip('\"')
+    # data = data.replace('"', '')
+    save_to_csv(metrics_path, [data])
 
 def save_to_csv(csv_path,data):
     with open(csv_path, "a", newline="") as file:
