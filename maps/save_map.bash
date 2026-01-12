771b16a06ff46caddbe5c9d81550cc7576765413
Connor <connor.york000@gmail.com>
2026-01-12 15:54:28 +0000

    switched back to amcl for localisation, new maps for testing

--- maps/save_map.bash
+++ maps/save_map.bash
@@ -9,13 +9,12 @@ fi
 
 MAP_NAME="$1"
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+FOLDER_NAME="${MAP_NAME^^}"
+mkdir -p "$DIR/$FOLDER_NAME"
 
-echo "Saving $MAP_NAME to $DIR"
-
+echo "Saving $MAP_NAME to $DIR/$FOLDER_NAME/"
 # Posegraph for localization
-rosservice call /slam_toolbox/serialize_map "filename: '$DIR/$MAP_NAME'"
-
+rosservice call /slam_toolbox/serialize_map "filename: '$DIR/$FOLDER_NAME/$MAP_NAME'"
 # PGM and YAML for keepout editing
-rosrun map_server map_saver -f "$DIR/$MAP_NAME" map:=/map
-
+rosrun map_server map_saver -f "$DIR/$FOLDER_NAME/$MAP_NAME" map:=/map
 echo "Done"
